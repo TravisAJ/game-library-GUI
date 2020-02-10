22f678c35c51061bc83825414006b77d9b595225
TravisAJ <tavenevoli@stcharlessd.org>
2020-02-10 13:17:10 -0600

    working on main menu

--- The Game Library.py
+++ The Game Library.py
@@ -4,13 +4,26 @@
 
 import pickle as pk
 import tkinter as tk
+from tkinter import scrolledtext
 
 '''The Game Library Program'''
 
 TITLE_FONT = ("Times New Roman", 24)
 BUTTON_FONT = ("Arial", 15)
 
-games = {}
-datafile = open("game_lib.pickle", "rb")
-games = pk.load(datafile)
-datafile.close()
+class MainMenu(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        lbl.title = tk.Label(text = "Game Library", font = TITLE_FONT)
+        lbl.title.grid(row = 0, column = 0, sticky = "news")
+
+if __name__ == "__main__":
+    games = {}
+    datafile = open("game_lib.pickle", "rb")
+    games = pk.load(datafile)
+    datafile.close()
+    root = tk.Tk()
+    root.title("The Game Library")
+    root.geometry("500x500")
+    
+    root.mainloop()
