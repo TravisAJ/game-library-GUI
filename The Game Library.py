2a9453db6abc59aa265c3c22d56402fc040f073d
TravisAJ <tavenevoli@stcharlessd.org>
2020-02-10 12:48:26 -0600

    added constants and imports

--- The Game Library.py
+++ The Game Library.py
@@ -6,3 +6,11 @@ import pickle as pk
 import tkinter as tk
 
 '''The Game Library Program'''
+
+TITLE_FONT = ("Times New Roman", 24)
+BUTTON_FONT = ("Arial", 15)
+
+games = {}
+datafile = open("game_lib.pickle", "rb")
+games = pk.load(datafile)
+datafile.close()
