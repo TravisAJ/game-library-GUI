0cd8627de6d6b655123cb2f82ebc4f61a0a2d899
TravisAJ <tavenevoli@stcharlessd.org>
2020-02-12 12:44:09 -0600

    working on view add screen

--- The Game Library.py
+++ The Game Library.py
@@ -14,29 +14,59 @@ ENTRY_FONT = ("Times New Roman", 15)
 BUTTON_FONT = ("Arial", 15)
 SCROLL_FONT = ("Arial", 12)
 
-'''class MainMenu(tk.Frame):
+class AddMenu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
-        lbl_title = tk.Label(text = "The Game Library", font = TITLE_FONT)
-        lbl_title.grid(row = 1, column = 1, sticky = "news")
+        self.lbl_genre = tk.Label(self, text = "Genre:", font = LABEL_FONT)
+        self.lbl_genre.grid(row = 1, column = 1, sticky = "news")
         
-        btn_add = tk.Button(text = "ADD", font = BUTTON_FONT)
-        btn_add.grid(row = 2, column = 1)
+        self.ent_genre = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_genre.grid(row = 1, column = 2, sticky = "news")
         
-        btn_edit = tk.Button(text = "EDIT", font = BUTTON_FONT)
-        btn_edit.grid(row = 3, column = 1)
+        self.lbl_title = tk.Label(self, text = "Title:", font = LABEL_FONT)
+        self.lbl_title.grid(row = 1, column = 3, sticky = "news")
         
-        btn_search = tk.Button(text = "SEARCH", command = raise_search, font = BUTTON_FONT)
-        btn_search.grid(row = 4, column = 1)
+        self.ent_title = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_title.grid(row = 1, column = 4, sticky = "news")
         
-        btn_remove = tk.Button(text = "REMOVE", font = BUTTON_FONT)
-        btn_remove.grid(row = 5, column = 1)
+        self.lbl_developer = tk.Label(self, text = "Genre:", font = LABEL_FONT)
+        self.lbl_developer.grid(row = 1, column = 5, sticky = "news")
         
-        btn_save = tk.Button(text = "SAVE", font = BUTTON_FONT)
-        btn_save.grid(row = 6, column = 1)
+        self.ent_developer = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_developer.grid(row = 1, column = 6, sticky = "news")
         
-    def raise_search(self):
-        search_menu.tkraise()'''
+        self.lbl_genre = tk.Label(self, text = "Genre:", font = LABEL_FONT)
+        self.lbl_genre.grid(row = 2, column = 1, sticky = "news")
+        
+        self.ent_genre = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_genre.grid(row = 2, column = 2, sticky = "news")
+        
+        self.lbl_genre = tk.Label(self, text = "Genre:", font = LABEL_FONT)
+        self.lbl_genre.grid(row = 2, column = 3, sticky = "news")
+        
+        self.ent_genre = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_genre.grid(row = 2, column = 4, sticky = "news")        
+
+class MainMenu(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.lbl_main = tk.Label(self, text = "The Game Library", font = TITLE_FONT)
+        self.lbl_main.grid(row = 1, column = 1, sticky = "news")
+        
+        self.btn_add = tk.Button(self, text = "ADD", font = BUTTON_FONT)
+        self.btn_add.grid(row = 2, column = 1, sticky = "news")
+        
+        self.btn_edit = tk.Button(self, text = "EDIT", font = BUTTON_FONT)
+        self.btn_edit.grid(row = 3, column = 1, sticky = "nwes")
+        
+        self.btn_search = tk.Button(self, text = "SEARCH", font = BUTTON_FONT)
+        self.btn_search.grid(row = 4, column = 1, sticky = "nwes")
+        
+        self.btn_remove = tk.Button(self, text = "REMOVE", font = BUTTON_FONT)
+        self.btn_remove.grid(row = 5, column = 1, sticky = "news")
+        
+        self.btn_save = tk.Button(self, text = "SAVE", font = BUTTON_FONT)
+        self.btn_save.grid(row = 6, column = 1, sticky = "news")
         
 class SearchMenu(tk.Frame):
     def __init__(self):
@@ -45,14 +75,17 @@ class SearchMenu(tk.Frame):
         self.lbl_search.grid(row = 1, column = 1, columnspan = 2, sticky = "news")
         
         self.lbl_search_by = tk.Label(self, text = "Search by:", font = LABEL_FONT)
-        self.lbl_search_by.grid(row = 2, column = 1, columnspan = 2, sticky = "news")
+        self.lbl_search_by.grid(row = 2, column = 1, columnspan = 2, sticky = "nsw")
         
-        '''Temporarily An Entry, Change to Dropdown'''
-        self.ent_search_by = tk.Entry(self, font = ENTRY_FONT)
-        self.ent_search_by.grid(row = 3, column = 1, columnspan = 2, sticky = "news")
+        options = ["Genre", "Title", "Developer", "Publisher", "System",
+                   "Release", "Rating", "Gamemode", "Price", "Progress", "Purchase"]
+        tkvar = tk.StringVar(self)
+        tkvar.set(options[0])
+        self.dpd_search_by = tk.OptionMenu(self, tkvar, * options)
+        self.dpd_search_by.grid(row = 3, column = 1, columnspan = 2, sticky = "news")
         
         self.lbl_search_for = tk.Label(self, text = "Search for:", font = LABEL_FONT)
-        self.lbl_search_for.grid(row = 4, column = 1, columnspan = 2, sticky = "news")
+        self.lbl_search_for.grid(row = 4, column = 1, columnspan = 2, sticky = "nsw")
         
         self.ent_search_for = tk.Entry(self, font = ENTRY_FONT)
         self.ent_search_for.grid(row = 5, column = 1, columnspan = 2, sticky = "news")
@@ -61,10 +94,10 @@ class SearchMenu(tk.Frame):
         self.lbl_print_filters.grid(row = 1, column = 4, columnspan = 4, sticky = "news")
             
         self.print_filters = PrintFilters(self)
-        self.print_filters.grid(row = 2, column = 4, columnspan = 3, rowspan = 4, sticky = "news")
+        self.print_filters.grid(row = 2, column = 4, columnspan = 3, rowspan = 4, sticky = "nsw")
         
-        self.scr_search = ScrolledText(self, font = SCROLL_FONT)
-        self.scr_search.grid(row = 6, column = 1, columnspan = 6, sticky = "news")
+        self.scr_search = ScrolledText(self, width = 60, height = 5, font = SCROLL_FONT)
+        self.scr_search.grid(row = 6, column = 1, columnspan = 6)
         
         self.btn_back = tk.Button(self, text = "BACK", font = BUTTON_FONT)
         self.btn_back.grid(row = 7, column = 1, columnspan = 2, sticky = "news")
@@ -78,41 +111,41 @@ class SearchMenu(tk.Frame):
 class PrintFilters(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, master = parent)
-        self.chk_genre = tk.Checkbutton(self, text = "Genre")
-        self.chk_genre.grid(row = 1, column = 1, sticky = "news")
+        self.chk_genre = tk.Checkbutton(self, fg = "gray" ,text = "Genre")
+        self.chk_genre.grid(row = 1, column = 1, sticky = "nsw")
         
-        self.chk_title = tk.Checkbutton(self, text = "Title")
-        self.chk_title.grid(row = 2, column = 1, sticky = "news")
+        self.chk_title = tk.Checkbutton(self, fg = "gray" ,text = "Title")
+        self.chk_title.grid(row = 2, column = 1, sticky = "nsw")
         
-        self.chk_developer = tk.Checkbutton(self, text = "Developer")
-        self.chk_developer.grid(row = 3, column = 1, sticky = "news")
+        self.chk_developer = tk.Checkbutton(self, fg = "gray" ,text = "Developer")
+        self.chk_developer.grid(row = 3, column = 1, sticky = "nsw")
         
-        self.chk_publisher = tk.Checkbutton(self, text = "Publisher")
-        self.chk_publisher.grid(row = 4, column = 1, sticky = "news")
+        self.chk_publisher = tk.Checkbutton(self, fg = "gray" ,text = "Publisher")
+        self.chk_publisher.grid(row = 4, column = 1, sticky = "nsw")
         
-        self.chk_system = tk.Checkbutton(self, text = "System")
-        self.chk_system.grid(row = 1, column = 2, sticky = "news")
+        self.chk_system = tk.Checkbutton(self, fg = "gray" ,text = "System")
+        self.chk_system.grid(row = 1, column = 2, sticky = "nsw")
         
-        self.chk_release = tk.Checkbutton(self, text = "Release")
-        self.chk_release.grid(row = 2, column = 2, sticky = "news")
+        self.chk_release = tk.Checkbutton(self, fg = "gray" ,text = "Release")
+        self.chk_release.grid(row = 2, column = 2, sticky = "nsw")
         
-        self.chk_rating = tk.Checkbutton(self, text = "Rating")
-        self.chk_rating.grid(row = 3, column = 2, sticky = "news")
+        self.chk_rating = tk.Checkbutton(self, fg = "gray" ,text = "Rating")
+        self.chk_rating.grid(row = 3, column = 2, sticky = "nsw")
         
-        self.chk_gamemode = tk.Checkbutton(self, text = "Gamemode")
-        self.chk_gamemode.grid(row = 4, column = 2, sticky = "news")
+        self.chk_gamemode = tk.Checkbutton(self, fg = "gray" ,text = "Gamemode")
+        self.chk_gamemode.grid(row = 4, column = 2, sticky = "nsw")
         
-        self.chk_price = tk.Checkbutton(self, text = "Price")
-        self.chk_price.grid(row = 1, column = 3, sticky = "news")
+        self.chk_price = tk.Checkbutton(self, fg = "gray" ,text = "Price")
+        self.chk_price.grid(row = 1, column = 3, sticky = "nsw")
         
-        self.chk_progress = tk.Checkbutton(self, text = "Progress")
-        self.chk_progress.grid(row = 2, column = 3, sticky = "news")
+        self.chk_progress = tk.Checkbutton(self, fg = "gray" ,text = "Progress")
+        self.chk_progress.grid(row = 2, column = 3, sticky = "nsw")
         
-        self.chk_purchase = tk.Checkbutton(self, text = "Purchase")
-        self.chk_purchase.grid(row = 3, column = 3, sticky = "news")
+        self.chk_purchase = tk.Checkbutton(self, fg = "gray" ,text = "Purchase")
+        self.chk_purchase.grid(row = 3, column = 3, sticky = "nsw")
         
-        self.chk_notes = tk.Checkbutton(self, text = "Notes")
-        self.chk_notes.grid(row = 4, column = 3, sticky = "news")                                                                                                
+        self.chk_notes = tk.Checkbutton(self, fg = "gray" ,text = "Notes")
+        self.chk_notes.grid(row = 4, column = 3, sticky = "nsw")
 
 if __name__ == "__main__":
     games = {}
@@ -122,11 +155,15 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("The Game Library")
     #root.geometry("500x500")
-    #main_menu = MainMenu()
-    #main_menu.grid(row = 0, column = 0, sticky = "news")   
     
-    search_menu = SearchMenu()
-    search_menu.grid(row = 0, column = 0, sticky = "news")
+    #main_menu = MainMenu()
+    #main_menu.grid(row = 0, column = 0, sticky = "nsw")   
+
+    add_menu = AddMenu()
+    add_menu.grid(row = 0, column = 0, sticky = "nsw")
+    
+    #search_menu = SearchMenu()
+    #search_menu.grid(row = 0, column = 0, sticky = "nsw")
     
     #main_menu.tkraise()
     root.mainloop()
