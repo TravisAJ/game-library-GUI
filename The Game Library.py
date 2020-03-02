2a6f703fb0ea49fc243c414dd1baf358d756aa84
TravisAJ <tavenevoli@stcharlessd.org>
2020-03-02 12:56:04 -0600

    Finished add and edit

--- The Game Library.py
+++ The Game Library.py
@@ -5,6 +5,7 @@
 import pickle as pk
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
 
 '''The Game Library Program'''
 
@@ -48,16 +49,17 @@ class MainMenu(Screen):
     
     def go_add(self):
         Screen.current = 1
+        screens[Screen.current].reset_add()
         Screen.switch_frame()
         
     def go_edit(self):
         popup = tk.Tk()
-        popup.title("Edit Menu")
-        frm_edit = EditMenu(popup)
+        popup.title("Edit")
+        frm_edit = EditPopup(popup)
         frm_edit.grid(row = 0, column = 0)
         
     def go_search(self):
-        Screen.current = 2
+        Screen.current = 3
         Screen.switch_frame()
         
     def go_remove(self):
@@ -144,10 +146,10 @@ class AddMenu(Screen):
         self.lbl_gamemode = tk.Label(self, text = "Gamemode:", font = LABEL_FONT)
         self.lbl_gamemode.grid(row = 4, column = 3, sticky = "news")
         
-        options = ["Singleplayer", "Multiplayer", "Either"]
-        tkvar = tk.StringVar(self)
-        tkvar.set(options[0])
-        self.dbx_gamemode = tk.OptionMenu(self, tkvar, * options)
+        options = ["** SELECT GAMEMODE **", "Singleplayer", "Multiplayer", "Either"]
+        self.tkvar = tk.StringVar(self)
+        self.tkvar.set(options[0])
+        self.dbx_gamemode = tk.OptionMenu(self, self.tkvar, * options)
         self.dbx_gamemode.grid(row = 4, column = 4, columnspan = 3, sticky = "news")
         
         self.scr_notes = ScrolledText(self, width = 110, height = 5, font = SCROLL_FONT)
@@ -176,12 +178,26 @@ class AddMenu(Screen):
         self.ent_rating.delete(0, 'end')
         self.ent_price.delete(0, 'end')
         self.ent_purchase.delete(0, 'end')
+        self.scr_notes.delete(0.0, 'end')
         
     def submit_add(self):
+        entry = []
+        entry.append(self.ent_genre.get())
+        entry.append(self.ent_title.get())
+        entry.append(self.ent_developer.get())
+        entry.append(self.ent_publisher.get())
+        entry.append(self.ent_system.get())
+        entry.append(self.ent_release.get())
+        entry.append(self.ent_rating.get())
+        entry.append(self.tkvar.get())
+        entry.append(self.ent_price.get())
+        entry.append("")
+        entry.append(self.ent_purchase.get())
+        entry.append(self.scr_notes.get(0.0, "end"))
+        games[len(games)+1] = entry
         Screen.current = 0
         Screen.switch_frame()
-        print("Game Added.")
-             
+                   
     def update(self):
         entry = games[self.edit_key]
         self.ent_genre.delete(0, "end")
@@ -201,9 +217,11 @@ class AddMenu(Screen):
         self.ent_price.delete(0, "end")
         self.ent_price.insert(0, entry[8])
         self.ent_purchase.delete(0, "end")
-        self.ent_purchase.insert(0, entry[10])       
+        self.ent_purchase.insert(0, entry[10])
+        self.scr_notes.delete(0.0, "end")
+        self.scr_notes.insert(0.0, entry[11])        
         
-class EditMenu(tk.Frame):
+class EditPopup(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, master = parent)
         self.parent = parent
@@ -227,7 +245,7 @@ class EditMenu(tk.Frame):
         self.btn_cancel_edit = tk.Button(self, text = "CANCEL", font = BUTTON_FONT, command = self.cancel_edit)
         self.btn_cancel_edit.grid(row = 4, column = 1, columnspan = 2, sticky = "news")
         
-        self.btn_edit = tk.Button(self, text = "EDIT", font = BUTTON_FONT, command = self.submit_edit)
+        self.btn_edit = tk.Button(self, text = "CONFIRM", font = BUTTON_FONT, command = self.submit_edit)
         self.btn_edit.grid(row = 4, column = 3, columnspan = 2, sticky = "news")    
         
     def cancel_edit(self):
@@ -239,13 +257,160 @@ class EditMenu(tk.Frame):
         else:
             for i in range(len(self.options)):
                 if self.tkvar.get() == self.options[i]:
-                    screens[1].edit_key = i
+                    screens[2].edit_key = i
                     break
-            Screen.current = 1
+            Screen.current = 2
             screens[Screen.current].update()
             Screen.switch_frame()        
             self.parent.destroy()
+
+class EditMenu(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+        self.edit_key = 0
         
+        self.grid_columnconfigure(1,weight=1)
+        self.grid_columnconfigure(2,weight=1)
+        self.grid_columnconfigure(3,weight=1)        
+        
+        self.lbl_genre = tk.Label(self, text = "Genre:", font = LABEL_FONT)
+        self.lbl_genre.grid(row = 1, column = 1, sticky = "news")
+        
+        self.ent_genre = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_genre.grid(row = 1, column = 2, sticky = "news")
+        
+        self.lbl_title = tk.Label(self, text = "Title:", font = LABEL_FONT)
+        self.lbl_title.grid(row = 1, column = 3, sticky = "news")
+        
+        self.ent_title = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_title.grid(row = 1, column = 4, sticky = "news")
+        
+        self.lbl_developer = tk.Label(self, text = "Developer:", font = LABEL_FONT)
+        self.lbl_developer.grid(row = 1, column = 5, sticky = "news")
+        
+        self.ent_developer = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_developer.grid(row = 1, column = 6, sticky = "news")
+        
+        self.lbl_publisher = tk.Label(self, text = "Publisher:", font = LABEL_FONT)
+        self.lbl_publisher.grid(row = 2, column = 1, sticky = "news")
+        
+        self.ent_publisher = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_publisher.grid(row = 2, column = 2, sticky = "news")
+        
+        self.lbl_system = tk.Label(self, text = "System:", font = LABEL_FONT)
+        self.lbl_system.grid(row = 2, column = 3, sticky = "news")
+        
+        self.ent_system = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_system.grid(row = 2, column = 4, sticky = "news")
+        
+        self.lbl_release = tk.Label(self, text = "Release Date:", font = LABEL_FONT)
+        self.lbl_release.grid(row = 2, column = 5, sticky = "news")
+        
+        self.ent_release = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_release.grid(row = 2, column = 6, sticky = "news")
+        
+        self.lbl_rating = tk.Label(self, text = "Rating:", font = LABEL_FONT)
+        self.lbl_rating.grid(row = 3, column = 1, sticky = "news")
+        
+        self.ent_rating = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_rating.grid(row = 3, column = 2, sticky = "news")
+        
+        self.lbl_price = tk.Label(self, text = "Price:", font = LABEL_FONT)
+        self.lbl_price.grid(row = 3, column = 3, sticky = "news")
+        
+        self.ent_price = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_price.grid(row = 3, column = 4, sticky = "news")
+        
+        self.lbl_purchase = tk.Label(self, text = "Purchase Date:", font = LABEL_FONT)
+        self.lbl_purchase.grid(row = 3, column = 5, sticky = "news")
+        
+        self.ent_purchase = tk.Entry(self, font = ENTRY_FONT)
+        self.ent_purchase.grid(row = 3, column = 6, sticky = "news")
+        
+        self.lbl_progress = tk.Label(self, text = "Beat It?", font = LABEL_FONT)
+        self.lbl_progress.grid(row = 4, column = 1, sticky = "news")
+        
+        self.chk_progress = tk.Checkbutton(self, fg = "gray")
+        self.chk_progress.grid(row = 4, column = 2, sticky = "nsw")
+        
+        self.lbl_gamemode = tk.Label(self, text = "Gamemode:", font = LABEL_FONT)
+        self.lbl_gamemode.grid(row = 4, column = 3, sticky = "news")
+        
+        options = ["** SELECT GAMEMODE **", "Singleplayer", "Multiplayer", "Either"]
+        self.tkvar = tk.StringVar(self)
+        self.tkvar.set(options[0])
+        self.dbx_gamemode = tk.OptionMenu(self, self.tkvar, * options)
+        self.dbx_gamemode.grid(row = 4, column = 4, columnspan = 3, sticky = "news")
+        
+        self.scr_notes = ScrolledText(self, width = 110, height = 5, font = SCROLL_FONT)
+        self.scr_notes.grid(row = 5, column = 1, columnspan = 6)
+        
+        self.btn_cancel_add = tk.Button(self, text = "CANCEL", font = BUTTON_FONT, command = self.cancel_edit)
+        self.btn_cancel_add.grid(row = 6, column = 1, columnspan = 2, sticky = "news")
+        
+        self.btn_reset = tk.Button(self, text = "RESET", font = BUTTON_FONT, command = self.reset_edit)
+        self.btn_reset.grid(row = 6, column = 3, columnspan = 2, sticky = "news")
+        
+        self.btn_add = tk.Button(self, text = "EDIT", font = BUTTON_FONT, command = self.submit_edit)
+        self.btn_add.grid(row = 6, column = 5, columnspan = 2, sticky = "news")
+        
+    def cancel_edit(self):
+        Screen.current = 0
+        Screen.switch_frame()
+        
+    def reset_edit(self):
+        self.ent_genre.delete(0, 'end')
+        self.ent_title.delete(0, 'end')
+        self.ent_developer.delete(0, 'end')
+        self.ent_publisher.delete(0, 'end')
+        self.ent_system.delete(0, 'end')
+        self.ent_release.delete(0, 'end')
+        self.ent_rating.delete(0, 'end')
+        self.ent_price.delete(0, 'end')
+        self.ent_purchase.delete(0, 'end')
+        self.scr_notes.delete(0.0, 'end')
+        
+    def submit_edit(self):
+        entry = []
+        entry.append(self.ent_genre.get())
+        entry.append(self.ent_title.get())
+        entry.append(self.ent_developer.get())
+        entry.append(self.ent_publisher.get())
+        entry.append(self.ent_system.get())
+        entry.append(self.ent_release.get())
+        entry.append(self.ent_rating.get())
+        entry.append(self.tkvar.get())
+        entry.append(self.ent_price.get())
+        entry.append("")
+        entry.append(self.ent_purchase.get())
+        entry.append(self.scr_notes.get(0.0, "end"))
+        games[self.edit_key] = entry
+        Screen.current = 0
+        Screen.switch_frame()
+                   
+    def update(self):
+        entry = games[self.edit_key]
+        self.ent_genre.delete(0, "end")
+        self.ent_genre.insert(0, entry[0])
+        self.ent_title.delete(0, "end")
+        self.ent_title.insert(0, entry[1])
+        self.ent_publisher.delete(0, "end")
+        self.ent_publisher.insert(0, entry[2])
+        self.ent_developer.delete(0, "end")
+        self.ent_developer.insert(0, entry[3])
+        self.ent_system.delete(0, "end")
+        self.ent_system.insert(0, entry[4])
+        self.ent_release.delete(0, "end")
+        self.ent_release.insert(0, entry[5])
+        self.ent_rating.delete(0, "end")
+        self.ent_rating.insert(0, entry[6])
+        self.ent_price.delete(0, "end")
+        self.ent_price.insert(0, entry[8])
+        self.ent_purchase.delete(0, "end")
+        self.ent_purchase.insert(0, entry[10])
+        self.scr_notes.delete(0.0, "end")
+        self.scr_notes.insert(0.0, entry[11])
+
 class SearchMenu(Screen):
     def __init__(self):
         Screen.__init__(self)
@@ -290,8 +455,8 @@ class SearchMenu(Screen):
         self.btn_clear = tk.Button(self, text = "CLEAR", font = BUTTON_FONT, command = self.clear_search)
         self.btn_clear.grid(row = 7, column = 3, columnspan = 2, sticky = "news")
         
-        self.btn_submit = tk.Button(self, text = "SUBMIT", font = BUTTON_FONT, command = self.submit_search)
-        self.btn_submit.grid(row = 7, column = 5, columnspan = 2, sticky = "news")
+        self.btn_search = tk.Button(self, text = "SEARCH", font = BUTTON_FONT, command = self.submit_search)
+        self.btn_search.grid(row = 7, column = 5, columnspan = 2, sticky = "news")
         
     def cancel_search(self):
         Screen.current = 0
@@ -303,7 +468,7 @@ class SearchMenu(Screen):
     def submit_search(self):
         Screen.current = 0
         Screen.switch_frame()
-        print("Searching Game...")        
+        messagebox.showinfo(message = "Search Function WIP")       
         
 class PrintFilters(tk.Frame):
     def __init__(self, parent):
@@ -379,7 +544,7 @@ class RemoveMenu(tk.Frame):
         
     def submit_remove(self):
         self.parent.destroy()
-        print("Game Removed.")
+        messagebox.showinfo(message = "Remove Function WIP") 
         
 class SaveMenu(tk.Frame):
     def __init__(self, parent):
@@ -410,11 +575,12 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("The Game Library")
     
-    screens = [MainMenu(), AddMenu(), SearchMenu()]
+    screens = [MainMenu(), AddMenu(), EditMenu(), SearchMenu()]
     
     screens[0].grid(row = 0, column = 0, sticky = "news")
     screens[1].grid(row = 0, column = 0, sticky = "news")
     screens[2].grid(row = 0, column = 0, sticky = "news")
+    screens[3].grid(row = 0, column = 0, sticky = "news")
 
     root.grid_columnconfigure(0, weight = 1)
     root.grid_rowconfigure(0, weight = 1)
--- reset.py
+++ reset.py
@@ -1,16 +1,43 @@
 import pickle
+import tkinter as tk
+from tkinter import messagebox
 
 games = {1:['Rhythm', 'Beat Saber', 'Beat Games', 'Oculus', 'Virtual Reality', 
             '2018', '10', 'Singleplayer', '30.00', 'No', '12/25/19', 'My favorite VR Game!'], 
          2:['Action', 'Just Shapes and Beats', 'Berzerk Studio', 'Steam', 'Windows',
             '2018', '9', 'Either', '20.00', 'Yes', '7/31/19', 'Great Music!'], 
          3:['MOBA', 'Brawl Stars', 'Supercell', 'Supercell', 'Android',
-            '2017', '8.5', 'Multiplayer', 'Free', '12/12/18', 'The best mobile game!'],
+            '2017', '8.5', 'Multiplayer', 'Free', 'Yes', '12/12/18', 'The best mobile game!'],
          4:['Action-Adventure', 'Lego Star Wars', 'Travellers Tales', 'LucasArts', 'Xbox One',
             '2007', '7', 'Single', '20.00', 'No', '1/10/20', 'Hmmmmmm....herrrrrmmmmm']
          }
 
-datafile = open("game_lib.pickle", "wb")
-pickle.dump(games, datafile)
-datafile.close()
-print("Data Reseted. Go Back to the Game Library.")
+LABEL_FONT = ("Times New Roman", 24)
+BUTTON_FONT = ("Times New Roman", 18)
+
+class Reset_Frame(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid_columnconfigure(1,weight=1)
+    
+        self.lbl_reset = tk.Label(self, text = "CONFIRM RESET?", font = LABEL_FONT)
+        self.lbl_reset.grid(row = 1, column = 1, sticky = "news")
+    
+        self.btn_reset = tk.Button(self, text = "Confirm", font = BUTTON_FONT, command = self.reset)
+        self.btn_reset.grid(row = 2, column = 1, sticky = "news")    
+
+    def reset(self):
+        datafile = open("game_lib.pickle", "wb")
+        pickle.dump(games, datafile)
+        datafile.close()
+        messagebox.showinfo(message = "Data Reseted. Go Back to the Game Library.")
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Data Reset")
+    
+main = Reset_Frame()
+main.grid(row = 0, column = 0, sticky = "news")
+main.tkraise()
+    
+root.mainloop()
